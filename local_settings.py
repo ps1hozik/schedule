351d351691fb8b289cda1fd8d41596a7a16225d8
psihozik <artemimennoi@yandex.by>
2025-05-13 17:46:12 +0300

    Added create teables

--- local_settings.py
+++ local_settings.py
@@ -1,7 +1,7 @@
 DATABASE = {
-    "NAME": "schedule_db",
-    "USER": "schedule_user",
-    "PASSWORD": "schedule_vsu",
+    "NAME": "your_db_name",
+    "USER": "your_db_user",
+    "PASSWORD": "your_db_user_password",
     "HOST": "localhost",
     "PORT": 3306,
 }
